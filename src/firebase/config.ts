5bba414aba7f61989edc2844c7f5a008c5ca31ab
Trader Chines <traderchinesvip@gmail.com>
2025-11-11 20:14:07 +0000

    seria isso?
    const firebaseConfig = {
      apiKey: "AIzaSyAbPL1QWgNf4CL9Afo

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,13 +2,12 @@
 // Você pode encontrar isso no seu console do Firebase em:
 // Configurações do projeto > Geral > Seus aplicativos > SDK setup and configuration > Config
 const firebaseConfig = {
-  apiKey: "COLE_SUA_API_KEY_AQUI",
-  authDomain: "SEU_AUTH_DOMAIN_AQUI",
-  projectId: "SEU_PROJECT_ID_AQUI",
-  storageBucket: "SEU_STORAGE_BUCKET_AQUI",
-  messagingSenderId: "SEU_MESSAGING_SENDER_ID_AQUI",
-  appId: "SEU_APP_ID_AQUI",
-  measurementId: "SEU_MEASUREMENT_ID_AQUI"
+  apiKey: "AIzaSyAbPL1QWgNf4CL9AfohqCQA9dAmOsOALMo",
+  authDomain: "day-trade-76hij.firebaseapp.com",
+  projectId: "day-trade-76hij",
+  storageBucket: "day-trade-76hij.appspot.com",
+  messagingSenderId: "920687532161",
+  appId: "1:920687532161:web:fa4dac5aafd6ed63125580"
 };
 
 export default firebaseConfig;
